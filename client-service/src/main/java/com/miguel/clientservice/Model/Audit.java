f1f2970e91010b9937f2e57a79122304ca581af5
MabMab^^ <miguelberrioh@hotmail.com>
2024-04-15 01:00:55 -0500

    Add model

---
+++ client-service/src/main/java/com/miguel/clientservice/Model/Audit.java
@@ -0,0 +1,16 @@
+package com.miguel.clientservice.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Audit {
+    private String id;
+    private String accion;
+    private Date fecha;
+}
